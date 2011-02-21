5e3ee7a75824d0e665fdecfe752cac05cf152333
Paul van der Walt <paul@denknerd.org>
2011-02-21 17:13:19 +0000

    Progress. Bib2HTML can now read an ATerm from parse-bib

--- bib2html/Main.hs
+++ bib2html/Main.hs
@@ -1,10 +1,17 @@
 module Main where
     
-    import CCO.Tree            (ATerm (App,List), Tree (fromTree, toTree),parser)
+    import CCO.Component hiding (parser)
+    import CCO.Feedback
+    import CCO.Tree            (ATerm (App,List), Tree (fromTree, toTree), parser)
     import Common.Types
+    import Common.TreeInstances
+    import Common.ATermUtils
+    import Control.Arrow
     
+    main :: IO ()
     main = ioWrap pipeline
    
+    pipeline :: Component String String
     pipeline =  parser        >>> 
                 aTerm2BibTex  {- >>>   
                 validator     >>>   
@@ -13,8 +20,15 @@ module Main where
                 html2Aterm    >>>   
                 aTerm2String
                 -}
+                >>> bibTex2Aterm >>> aTerm2String
    
+
     aTerm2BibTex :: Component ATerm BibTex
     aTerm2BibTex = component toTree
  
   
+    --TEMP
+    bibTex2Aterm :: Component BibTex ATerm
+    bibTex2Aterm = component $ (\inp -> do trace_ "Flattening tree..."
+                                           (return . fromTree) inp)
+
--- common/TreeInstances.hs
+++ common/TreeInstances.hs
@@ -1,20 +1,25 @@
 module Common.TreeInstances where
     
     import CCO.Tree            (ATerm (App,List,String), Tree (fromTree, toTree))
+    import CCO.Tree.Parser
     import Common.Types
+    import Control.Applicative
 
     instance Tree BibTex where
         fromTree (BibTex l) = App "BibTex" [List (map fromTree l)]
+        toTree = parseTree [app "BibTex" (BibTex <$> arg)]
 
     instance Tree Entry where
         fromTree e = App "Entry" [ fromTree $ entryType e         -- type
                                  , fromTree $ reference e         -- ref
                                  , List $ map fromTree (fields e) -- fields 
                                  ] 
+        toTree = parseTree [app "Entry" (Entry   <$> arg <*> arg <*> arg)]
 
 
     instance Tree Field where
         fromTree (Field k v) = App "Field" [ String k -- key 
                                            , String v -- value
                                            ]
+        toTree = parseTree [app "Field" (Field <$> arg <*> arg)]
 
