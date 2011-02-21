5c6a739f130775c48145592c9c351d80fc485f4c
Paul van der Walt <paul@denknerd.org>
2011-02-21 16:54:51 +0000

    same rename

--- bib2html/Main.hs
+++ bib2html/Main.hs
@@ -1,14 +1,18 @@
 module Main where
     
+    import CCO.Tree            (ATerm (App,List), Tree (fromTree, toTree),parser)
+    import Common.Types
+    
     main = ioWrap pipeline
    
     pipeline =  parser        >>> 
-                aTerm2BibTex  >>>   
+                aTerm2BibTex  {- >>>   
                 validator     >>>   
                 sorter        >>>   
                 bibTex2Html   >>>   
                 html2Aterm    >>>   
                 aTerm2String
+                -}
    
     aTerm2BibTex :: Component ATerm BibTex
     aTerm2BibTex = component toTree
