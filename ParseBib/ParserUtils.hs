f74d6888aeb90b68a2eb65618853197050042d44
Paul van der Walt <paul@denknerd.org>
2011-02-22 22:06:44 +0000

    whee doc

--- ParseBib/ParserUtils.hs
+++ ParseBib/ParserUtils.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 
+-- | This module contains some useful parsing utilities.
 module ParseBib.ParserUtils where
 
     import Text.ParserCombinators.UU
@@ -12,13 +13,15 @@ module ParseBib.ParserUtils where
     -- | Parse a given input using some UU-Parsinglib parser, but
     -- return feedback in the CCO Feedback monad. Useful for reporting
     -- failures and error corrections done by the parser.
-    parseFeedback :: Parser BibTex -> String -> Feedback BibTex
+    parseFeedback :: Parser BibTex -- ^ the parser to use
+                  -> String        -- ^ the input
+                  -> Feedback BibTex -- ^ return a BibTex, but also allow Feedback
     parseFeedback p inp = do trace_ "Parsing .bib file..."
                              let r@(a, errors) =  parse ( (,) <$> p <*> pEnd) (listToStr inp (0,0))
-                             if null errors then  do trace_ "Parsed without errors."
-                                                     return a 
+                             if null errors then  return ()
                                             else  do sequence $ map (warn_ . show) errors
-                                                     return a 
+                                            	     return ()
+                             return a 
 
     -- | the pMunch1 parser is much like pMunch, except that it only 
     -- succeeds when it can munch 1 or more characters. It fails on 
@@ -35,7 +38,7 @@ module ParseBib.ParserUtils where
     pBraced :: Parser a -> Parser a
     pBraced a = pSym '{' *> a <* pSym '}'
 
-    -- | A parser which greedily consumes whitespace.
+    -- | A parser which greedily consumes whitespace. Useful between entries or fields. 
     spaces :: Parser String
     spaces = pMunch (flip elem " \t\n")
 
--- ParseBib/Tool.hs
+++ ParseBib/Tool.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+--  | This module defines the initial tool, parse-bib. It is responsible for filtering comments, 
+--  parsing the input, and creating the first ATerm out of the BibTex tree, once parsed.
 module ParseBib.Tool where
     
     import CCO.Feedback
@@ -13,27 +15,34 @@ module ParseBib.Tool where
     import ParseBib.Parser
     import ParseBib.ParserUtils
 
+    -- | wrap the pipeline in IO and run.
     mainFunc :: IO ()
     mainFunc = ioWrap pipeline
 
+    -- | the pipeline which carries out the aforementioned steps. 
     pipeline :: Component String String
     pipeline =  removeComments >>>
                 bibTexparser  >>>   
                 bibTex2Aterm  >>>   
                 aTerm2String 
 
+    -- | removeComments splits the input into lines, then processes each line
+    -- for comments using the killComments function
     removeComments :: Component String String
     removeComments = component $ (\inp -> do let splitfile = lines inp
                                              let rawFile = map killComments splitfile
                                              return $ unlines rawFile
                                  )
 
+    -- | killComments throws away everything after a '%' on a line of input.
     killComments :: String -> String
     killComments = takeWhile (/= '%')
 
+    -- | run the parser, parseBib, on the input, using the parseFeedback wrapper.
     bibTexparser :: Component String BibTex
     bibTexparser = component $ parseFeedback parseBib
 
+    -- | convert the parsed BibTex tree into an ATerm.
     bibTex2Aterm :: Component BibTex ATerm
     bibTex2Aterm = component $ (\inp -> do trace_ "Flattening tree..."
                                            (return . fromTree) inp)
